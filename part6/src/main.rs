00c0b201c84bd6df37260b4077f355d738e61611
yc-wang00 <yicheng.wang.2023@gmail.com>
2024-01-23 22:35:23 +0000

    chore: part 1. 6 solution

--- part6/src/main.rs
+++ part6/src/main.rs
@@ -1,45 +1,37 @@
 fn main() {
     let numbers = vec![2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24];
 
-    let sum_of_nums = sum(numbers);
-    let product_of_nums = 0; // 👉 TODO call product() and put answer here
-    let average_of_nums = 0; // 👉 TODO call average() and put answer here
-
-    // 💡 TIP: You'll get a compile error. Here are two ways you can fix it:
-    //
-    // Option 1: Accept &Vec<i64> instead of Vec<i64>
-    //
-    // Option 2: Accept a slice - that is, &[i64] - instead of a Vec
-    //
-    // Give both options a try!
+    let (sum_of_nums, numbers) = sum(numbers);
+    let (product_of_nums, numbers) = product(numbers);
+    let average_of_nums = average(numbers);
 
     println!("Sum of these numbers: {}", sum_of_nums);
     println!("Product of these numbers: {}", product_of_nums);
     println!("Average of these numbers: {}", average_of_nums);
 }
 
-fn sum(numbers: Vec<i64>) -> i64 {
+fn sum(numbers: Vec<i64>) -> (i64, Vec<i64>) {
     let mut total = 0;
 
     for num in numbers.iter() {
         total += num;
     }
 
-    total
+    (total, numbers)
 }
 
-fn product(numbers: Vec<i64>) -> i64 {
+fn product(numbers: Vec<i64>) -> (i64, Vec<i64>) {
     let mut total = 1;
 
     for num in numbers.iter() {
         total *= num;
     }
 
-    total
+    (total, numbers)
 }
 
 fn average(numbers: Vec<i64>) -> i64 {
     let length = numbers.len() as i64;
 
-    sum(numbers) / length
+    sum(numbers).0 / length
 }
